28a8f369f16a963278b123209d216803d744435f
Omar Shafik <o.shafik13@gmail.com>
2024-01-31 14:44:43 +0200

    add rate and edge discriminability

--- src/procedures/surrogate_analysis.py
+++ src/procedures/surrogate_analysis.py
@@ -323,26 +323,53 @@ def analyze_surrogate_statistics(
                 f"{discriminability_index_h2}", results_dirname)
             print_info(f"INFO: Disriminability index of H1 & H2 (w={window_size}): " + \
                 f"{discriminability_index_h1h2}", results_dirname)
-            edge_discriminability_h1 = (np.sum(
+            rate_discriminability_h1 = (np.sum(
                 edge_h1_significance_rate[edge_h1_significance_rate > 1]
             ) + np.sum(
                 false_significance_rate[false_significance_rate < 1]
             )) / (
                 np.sum(edge_h1_significance_rate) + np.sum(false_significance_rate)
             )
-            edge_discriminability_h2 = (np.sum(
+            rate_discriminability_h2 = (np.sum(
                 edge_h2_significance_rate[edge_h2_significance_rate > 1]
             ) + np.sum(
                 false_significance_rate[false_significance_rate < 1]
             )) / (
                 np.sum(edge_h2_significance_rate) + np.sum(false_significance_rate)
             )
-            edge_discriminability_h1h2 = (np.sum(
+            rate_discriminability_h1h2 = (np.sum(
                 edge_h1h2_significance_rate[edge_h1h2_significance_rate > 1]
             ) + np.sum(
                 false_significance_rate[false_significance_rate < 1]
             )) / (
                 np.sum(edge_h1h2_significance_rate) + np.sum(false_significance_rate)
+            )
+            print_info(f"INFO: Significance rate disriminability index of H1 (w={window_size}): " + \
+                f"{rate_discriminability_h1}", results_dirname)
+            print_info(f"INFO: Significance rate disriminability index of H2 (w={window_size}): " + \
+                f"{rate_discriminability_h2}", results_dirname)
+            print_info(f"INFO: Significance rate disriminability index of H1 & H2 (w={window_size}): " + \
+                f"{rate_discriminability_h1h2}", results_dirname)
+            edge_discriminability_h1 = (np.sum(
+                edge_h1_significance_rate[edge_h1_significance_rate > 1]
+            ) + np.sum(
+                np.where(false_significance_rate[false_significance_rate < 1] > 1, 1, 0)
+            )) / (
+                edge_h1_significance_rate.shape[0] + false_significance_rate.shape[0]
+            )
+            edge_discriminability_h2 = (np.sum(
+                edge_h2_significance_rate[edge_h2_significance_rate > 1]
+            ) + np.sum(
+                np.where(false_significance_rate[false_significance_rate < 1] > 1, 1, 0)
+            )) / (
+                edge_h2_significance_rate.shape[0] + false_significance_rate.shape[0]
+            )
+            edge_discriminability_h1h2 = (np.sum(
+                edge_h1h2_significance_rate[edge_h1h2_significance_rate > 1]
+            ) + np.sum(
+                np.where(false_significance_rate[false_significance_rate < 1] > 1, 1, 0)
+            )) / (
+                edge_h1h2_significance_rate.shape[0] + false_significance_rate.shape[0]
             )
             print_info(f"INFO: Edge disriminability index of H1 (w={window_size}): " + \
                 f"{edge_discriminability_h1}", results_dirname)
