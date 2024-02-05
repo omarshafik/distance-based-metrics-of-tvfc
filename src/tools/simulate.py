changes - use node-averaged spectrum magnitude for surrogates - fix null significance rate distributions

src/tools/simulate.py
@@ -3,6 +3,7 @@ import tools
 
 def sc(
     empirical_data,
+    average_spectrum=True,
     random: np.random.Generator = None):
     """
     Generate spectrally-constrained surrogate data from empirical data.
@@ -23,6 +24,8 @@ def sc(
         random = np.random
     empirical_fft = np.fft.fft(empirical_data, axis=-1)
     empirical_fft_amplitude = np.abs(empirical_fft)
+    if average_spectrum:
+        empirical_fft_amplitude = np.mean(empirical_fft_amplitude, axis=0)
     noise = random.randn(*empirical_data.shape)
     random_phases = np.angle(np.fft.fft(noise))
     simulated_spectrum = empirical_fft_amplitude \
@@ -33,7 +36,7 @@ def sc(
 
 def pr(
     empirical_data,
-    average_spectrum=False,
+    average_spectrum=True,
     random: np.random.Generator = None):
     """
     Generate phase-randomized surrogate data based on the empirical data
@@ -51,7 +54,7 @@ def pr(
     empirical_fft = np.fft.fft(empirical_data, axis=-1)
     empirical_fft_amplitude = np.abs(empirical_fft)
     if average_spectrum:
-        np.mean(empirical_fft_amplitude, axis=0)
+        empirical_fft_amplitude = np.mean(empirical_fft_amplitude, axis=0)
     noise = random.randn(empirical_data.shape[-1])
     random_phases = np.angle(np.fft.fft(noise))
     # random_phases = random.uniform(low=-np.pi, high=np.pi, size=empirical_data.shape[1])
@@ -64,6 +67,7 @@ def pr(
 
 def laumann(
     empirical_data,
+    average_spectrum=True,
     random: np.random.Generator = None):
     """_summary_
 
@@ -75,12 +79,7 @@ def laumann(
     """
     if random is None:
         random = np.random
-    noise = random.randn(*empirical_data.shape)
-    random_phases = np.angle(np.fft.fft(noise))
-    power_spectrum = np.mean(np.abs(np.fft.fft(empirical_data, axis=-1)), axis=0)
-    simulated_spectrum = power_spectrum \
-        * np.exp(1j * random_phases)
-    sc_data = np.fft.ifft(simulated_spectrum, axis=-1).real
+    sc_data = sc(empirical_data, average_spectrum=average_spectrum, random=random)
     emp_cov = np.cov(empirical_data)
     chol_decomposition = np.linalg.cholesky(emp_cov)
     laumann_data = np.dot(chol_decomposition, sc_data)

src/tools/test.py
@@ -177,7 +177,7 @@ def sdr(
     significance_rate_of_interest: float,
     false_significance_rate: float,
     null_distribution: np.ndarray = None,
-    alpha: float = 0.05):
+    alpha: float = 0.1):
     """get the significance rate discriminability (SRD) rate
 
     Args:
@@ -190,11 +190,9 @@ def sdr(
     null_rate_upper_bound = 1
     null_rate_lower_bound = 1
     if null_distribution is not None:
-        null_rate_lower_bound, null_rate_upper_bound = stats.norm.interval(
-            (1 - alpha),
-            loc=np.mean(null_distribution),
-            scale=np.std(null_distribution)
-        )
+        null_rate_lower_bound = np.percentile(null_distribution, 100 * (alpha // 2))
+        null_rate_upper_bound = np.percentile(null_distribution, 100 * (1 - alpha // 2))
+
     statistic = (
         np.sum(
             significance_rate_of_interest[significance_rate_of_interest > null_rate_upper_bound]
@@ -223,11 +221,8 @@ def edr(
     null_rate_upper_bound = 1
     null_rate_lower_bound = 1
     if null_distribution is not None:
-        null_rate_lower_bound, null_rate_upper_bound = stats.norm.interval(
-            (1 - alpha),
-            loc=np.mean(null_distribution),
-            scale=np.std(null_distribution)
-        )
+        null_rate_lower_bound = np.percentile(null_distribution, 100 * (alpha // 2))
+        null_rate_upper_bound = np.percentile(null_distribution, 100 * (1 - alpha // 2))
 
     statistic = (
         (